a1161bc0d45aff07d72afa081697021fd77c6d41
gcamargocamposrocha <gcamargocamposrocha@guidewire.com>
2022-03-08 13:16:54 +0100

    Configure React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { lazy, Suspense } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+const SignIn = lazy(() => import("./pages/SignIn/SignIn"));
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Container component="main">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<SignIn />} />
+          </Routes>
+        </Suspense>
+      </Container>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
